91e440c3036ebec1472a174a609428b1b385c7ec
jypark38 <jury124@naver.com>
2024-02-04 16:44:51 +0900

    todoList Selector

--- redux-fundamentals-example-app-master/src/features/todos/TodoList.jsx
+++ redux-fundamentals-example-app-master/src/features/todos/TodoList.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import TodoListItem from './TodoListItem'
 
 const TodoList = () => {
-  return <div>TodoList</div>
+  const todos = useSelector((state) => state.todos)
+
+  const renderedListItems = todos.map((todo) => {
+    return <TodoListItem key={todo.id} todo={todo} />
+  })
+  return <ul className="todo-list">{renderedListItems}</ul>
 }
 
 export default TodoList
