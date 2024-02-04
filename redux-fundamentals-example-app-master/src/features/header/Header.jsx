2679aabffdcf3d3f0d89371a97355ea3341b868e
jypark38 <jury124@naver.com>
2024-02-04 16:56:56 +0900

    useDispatch()

--- redux-fundamentals-example-app-master/src/features/header/Header.jsx
+++ redux-fundamentals-example-app-master/src/features/header/Header.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Header = () => {
-  return <div>Header</div>
+  const [text, setText] = useState('')
+  const dispatch = useDispatch()
+
+  const handleChange = (event) => setText(event.target.value)
+
+  const handleKeyDown = (event) => {
+    const trimmedText = event.target.value.trim()
+
+    if (event.key === 'Enter' && trimmedText) {
+      const action = {
+        type: 'todos/todoAdded',
+        payload: trimmedText,
+      }
+      dispatch(action)
+      setText('')
+    }
+  }
+
+  return (
+    <input
+      type="text"
+      placeholder="What needs to be done?"
+      autoFocus={true}
+      value={text}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  )
 }
 
 export default Header
