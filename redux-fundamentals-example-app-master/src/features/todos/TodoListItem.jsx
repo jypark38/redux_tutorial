6c0c8abc1efec57b068818cbe5079f590c7f9bbe
jypark38 <jury124@naver.com>
2024-02-06 17:02:27 +0900

    shallow Equal

--- redux-fundamentals-example-app-master/src/features/todos/TodoListItem.jsx
+++ redux-fundamentals-example-app-master/src/features/todos/TodoListItem.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { availableColors, capitalize } from '../filters/colors'
+import { ReactComponent as TimesSolid } from './times-solid.svg'
 
-const TodoListItem = ({ todo }) => {
-  return <div>TodoListItem</div>
+const selectTodoById = (state, todoId) => {
+  return state.todos.find((todo) => todo.id === todoId)
+}
+
+const TodoListItem = ({ id }) => {
+  const todo = useSelector((state) => selectTodoById(state, id))
+  const { text, completed, color } = todo
+
+  const dispatch = useDispatch()
+
+  const handleCompletedChanged = () => {
+    dispatch({ type: 'todos/todoToggled', payload: todo.id })
+  }
+
+  const handleColorChanged = (event) => {
+    const color = event.target.value
+    dispatch({
+      type: 'todos/colorSelected',
+      payload: { todoId: todo.id, color },
+    })
+  }
+
+  const onDelete = () => {
+    dispatch({ type: 'todos/todoDeleted', payload: todo.id })
+  }
+
+  const colorOptions = availableColors.map((c) => (
+    <option value={c} key={c}>
+      {capitalize(c)}
+    </option>
+  ))
+
+  return (
+    <li>
+      <div className="view">
+        <div className="segment label">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={completed}
+            onChange={handleCompletedChanged}
+          />
+          <div className="todo-text">{text}</div>
+        </div>
+        <div className="segment buttons">
+          <select
+            className="colorPicker"
+            value={color}
+            style={{ color }}
+            onChange={handleColorChanged}
+          >
+            {/* 초기상태 */}
+            <option value=""></option>
+            {colorOptions}
+          </select>
+          <button className="destroy" onClick={onDelete}>
+            <TimesSolid />
+          </button>
+        </div>
+      </div>
+    </li>
+  )
 }
 
 export default TodoListItem
