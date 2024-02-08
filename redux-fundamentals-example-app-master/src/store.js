204d9342fb8a3519072035707038f48271717297
jypark38 <jury124@naver.com>
2024-02-08 16:50:49 +0900

    redux toolkit configureStore

--- redux-fundamentals-example-app-master/src/store.js
+++ redux-fundamentals-example-app-master/src/store.js
@@ -1,13 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducers'
+import { configureStore } from '@reduxjs/toolkit'
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const composedEnhancer = composeWithDevTools(
-  // Add whatever middleware you actually want to use here
-  applyMiddleware(thunkMiddleware)
-  // other store enhancers if any
-)
-const store = createStore(rootReducer, composedEnhancer)
+const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filtersReducer,
+  },
+})
 
 export default store
