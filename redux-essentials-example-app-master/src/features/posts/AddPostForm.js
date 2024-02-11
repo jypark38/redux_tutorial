6d84bb4839c2babb230d2bdc29091602a2e008f0
jypark38 <jury124@naver.com>
2024-02-11 18:50:12 +0900

    리듀서 추가 + 게시글 추가 기능 부착

---
+++ redux-essentials-example-app-master/src/features/posts/AddPostForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
+import { postAdded } from './postsSlice'
+
+const AddPostForm = () => {
+  const dispatch = useDispatch()
+
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+
+  const onSavePostClicked = (e) => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        }),
+      )
+
+      setTitle('')
+      setContent('')
+    }
+  }
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form action="">
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  )
+}
+
+export default AddPostForm
