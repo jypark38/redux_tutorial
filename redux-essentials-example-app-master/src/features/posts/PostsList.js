6881d838d6ec7454bee9a333af92a33c73b6766a
jypark38 <jury124@naver.com>
2024-02-12 17:55:11 +0900

    async thunk

--- redux-essentials-example-app-master/src/features/posts/PostsList.js
+++ redux-essentials-example-app-master/src/features/posts/PostsList.js
@@ -1,19 +1,14 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import PostAuthor from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import ReactionButtons from './ReactionButtons'
-import { selectAllPosts } from './postsSlice'
+import { fetchPosts, selectAllPosts } from './postsSlice'
+import { Spinner } from '../../components/Spinner'
 
-const PostsList = () => {
-  // 리렌더링 최적화하려면 메모이징 셀렉터를 따로 만들어야 할듯?
-  const posts = useSelector(selectAllPosts)
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
-
-  const renderedPosts = orderedPosts.map((post) => (
+const PostExcerpt = ({ post }) => {
+  return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <div>
@@ -21,17 +16,46 @@ const PostsList = () => {
         <TimeAgo timestamp={post.date} />
       </div>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
-      <ReactionButtons post={post} />
     </article>
-  ))
+  )
+}
+
+const PostsList = () => {
+  const dispatch = useDispatch()
+
+  // 리렌더링 최적화하려면 메모이징 셀렉터를 따로 만들어야 할듯?
+  const posts = useSelector(selectAllPosts)
+  const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => <PostExcerpt post={post} />)
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
